810c00f0caf184d4c813c14ea50910cf22114ea8
havelockau <124767852+havelockau@users.noreply.github.com>
2023-02-08 17:01:22 +0800

    Updated settings.ts
    Oops, its not js

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   site: 'https://havelock.cc',
-  name: 'Astro Simple Blog',
+  name: 'Astro - Simple Blog',
   title: 'HAVELOCK',
   description: 'Perth based cybersecurity analyst blog',
   showLittleSticksPlug: false,
