f6747dde33a02a1d191f04fb8e1801941a3b5e59
Havelock <wlawson@havelock.cc>
2023-02-17 14:29:48 +0800

    Removed github social

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -5,11 +5,6 @@ export const socials = [
     icon: 'mdi:twitter'
   },
   {
-    name: 'GitHub',
-    url: 'https://github.com/havelockau',
-    icon: 'mdi:github'
-  },
-  {
     name: 'Instagram',
     url: 'https://instagram.com/w1l.l',
     icon: 'mdi:instagram'
